7bbdf7339d17901cc50865ac98a89bfb3590b4ca
s-hiai <satoshi.hiai@gmail.com>
2021-12-16 20:49:56 +0900

    add prac_36.py

---
+++ prac_36.py
@@ -0,0 +1,17 @@
+N, W = map(int, input().split())
+vw_list = [tuple(map(int, input().split())) for _ in range(N)]
+dp = [[0 for _ in range(W+1)] for _ in range(N+1)]
+
+for i in range(N):
+    vi, wi = vw_list[i]
+    for w in range(W+1):
+        if w - wi >= 0:
+            dp[i+1][w] = max(dp[i][w-wi]+vi, dp[i][w], dp[i+1][w-wi]+vi)
+        else:
+            dp[i+1][w] = dp[i][w]
+print(dp[N][W])
+
+
+
+
+
